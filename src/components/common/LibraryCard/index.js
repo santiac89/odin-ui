4b10b8cee843811c2bc6c405540ba38a4e284d08
Santiago Carullo <scarullo@elementum.com>
2017-07-30 18:13:34 -0300

    Style library cards

--- src/components/common/LibraryCard/index.js
+++ src/components/common/LibraryCard/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import {Card, CardTitle, CardActions} from 'material-ui/Card';
+import React, { Component } from 'react';
+import {Card, CardTitle, CardMedia} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
 import config from '../../../config';
 
 import './style.css';
 
-const LibraryCard = ({ name, path, image, startStreaming }) => (
-  <Card className="library-card">
-    <img src={`http://${config.odin.host}:${config.odin.port}${image}`} />
-    <CardTitle title={name} titleStyle={{ fontSize: 15 }} />
-    <CardActions className='card-actions'>
-      <FlatButton label="Watch" primary={true} onTouchTap={() => startStreaming(path)} />
-    </CardActions>
-  </Card>
-);
+export default class LibraryCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { imgFailed: false }
+  }
 
-export default LibraryCard;
+  onError = () => {
+    this.setState({ imgFailed: true })
+  }
+
+  render() {
+    const { name, image, path, startStreaming } = this.props;
+    let cardContent;
+
+    if (!this.state.imgFailed) {
+      cardContent = (
+        <CardMedia className="overlay" overlay={
+          <CardTitle title={name} titleStyle={{ fontSize: 15 }} />}>
+           <img
+            src={`http://${config.odin.host}:${config.odin.port}${image}`}
+            onError={this.onError}
+            alt={`http://${config.odin.host}:${config.odin.port}${image}`}
+           />
+          <FlatButton labelStyle={{ fontSize: '50px' }} className='watch-button' label="&#9658;"  onTouchTap={() => startStreaming(path)} />
+        </CardMedia>
+      )
+    } else {
+      cardContent = (
+        <CardMedia className="overlay" overlay={<CardTitle title={name} titleStyle={{ fontSize: 15 }} />}>
+          <img src="/placeholder.png" alt='placeholder' />
+          <FlatButton labelStyle={{ fontSize: '50px' }} className='watch-button' label="&#9658;" onTouchTap={() => startStreaming(path)} />
+        </CardMedia>
+      )
+    }
+
+    return (<Card className="library-card">{cardContent}</Card>)
+  }
+}
